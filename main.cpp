cbcdfb32f2ddd204870ed92aad7e73363a133855
Cristian Herghelegiu <herr.cristi@gmail.com>
2024-10-21 20:54:00 +0300

    Config project

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <condition_variable>
+#include <cstdio>
+#include <iostream>
+#include <map>
+#include <thread>
+#include <unistd.h>
+
+// make sure the path is included correct
+#include "include/base64.h"
+
+int main()
+{
+    return 0;
+}
