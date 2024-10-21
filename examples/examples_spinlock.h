c78ba546a6971001e167c877c9e113b7108548cc
Cristian Herghelegiu <herr.cristi@gmail.com>
2024-10-21 21:56:06 +0300

    Config project

---
+++ examples/examples_spinlock.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <condition_variable>
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+
+// make sure the path is included correct
+#include "../include/spinlock.h"
+
+namespace examples::spinlock {
+
+    //
+    // spin lock example 1
+    //
+    int Example1()
+    {
+        std::cout << "Spin lock\n";
+
+        // thread function
+        auto fn_t = [](auto i, small::spinlock &lock) {
+            for (int t = 0; t < 5; ++t) {
+                {
+                    std::unique_lock<small::spinlock> mlock(lock);
+                    std::cout << "spin lock thread=" << i << ", iteration=" << t << "\n";
+                }
+                std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            }
+        };
+
+        // spinlock
+        small::spinlock lock;
+        // create threads
+        std::array<std::thread, 3> threads;
+        for (size_t i = 0; i < threads.size(); ++i) {
+            threads[i] = std::thread(fn_t, i, std::ref(lock));
+        }
+
+        // wait
+        for (auto &t : threads) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+
+        std::cout << "spin lock finishing\n\n";
+
+        return 0;
+    }
+} // namespace examples
--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@
 #include <unistd.h>
 
 // make sure the path is included correct
-#include "include/base64.h"
+#include "examples/examples_spinlock.h"
 
 int main()
 {
+    std::cout << "Hello. Here are the default small cpp utils examples\n\n";
+
+    examples::spinlock::Example1();
+
     return 0;
 }
