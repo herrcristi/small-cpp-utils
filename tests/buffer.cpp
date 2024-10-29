a81171a65d18dfa112114cb0e7ec6c3858aaf802
Cristian Herghelegiu <herr.cristi@gmail.com>
2024-10-29 19:43:56 +0200

    Add unit tests

--- tests/buffer.cpp
+++ tests/buffer.cpp
@@ -35,7 +35,7 @@ namespace {
         small::buffer b2{"abc"};
         ASSERT_EQ(b2, "abc");
 
-        small::buffer b3("abc", 2);
+        small::buffer b3{"abc", 2};
         ASSERT_EQ(b3, "ab");
 
         constexpr std::string_view sv = "abc";
@@ -80,6 +80,57 @@ namespace {
         ASSERT_EQ(b5, "abc");
         b5.set_chunk_size(1UL);
         ASSERT_EQ(b5.get_chunk_size(), 1);
+    }
+
+    TEST_F(BufferTest, buffer_extract)
+    {
+        struct AutoDelete
+        {
+            char *m_e{};
+            explicit AutoDelete(char *e) : m_e(e) {}
+            ~AutoDelete() { small::buffer::free(m_e); };
+        };
+
+        small::buffer b;
+        b = m_test;
+        ASSERT_EQ(b, m_test);
+
+        auto e = b.extract();
+        AutoDelete ad(e);
+
+        ASSERT_EQ(e, m_test);
+        ASSERT_EQ(b, "");
+
+        small::buffer b1;
+        ASSERT_EQ(b1, "");
+
+        auto e1 = b1.extract();
+        AutoDelete ad1(e1);
+        b1 += 'a';
+
+        ASSERT_EQ(b1, "a");
+        ASSERT_EQ(e1, std::string());
+    }
+
+    TEST_F(BufferTest, buffer_plus)
+    {
+        small::buffer b;
+        b = m_test;
+        ASSERT_EQ(b, m_test);
+        ASSERT_EQ(b.get_chunk_size(), small::DEFAULT_BUFFER_CHUNK_SIZE);
+
+        b += 'a';
+        ASSERT_EQ(b, m_test + "a");
+
+        b += "b";
+        ASSERT_EQ(b, m_test + "ab");
+
+        b += {"c", 1};
+        ASSERT_EQ(b, m_test + "abc");
+
+        constexpr std::string_view sv = "d";
+        b += {sv};
+        ASSERT_EQ(b, m_test + "abcd");
     }
 
 } // namespace
