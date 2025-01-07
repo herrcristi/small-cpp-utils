b4a8feef3db1e7c08c0f2ca9dfcf34107d65c220
Cristian Herghelegiu <herr.cristi@gmail.com>
2025-01-07 21:04:50 +0200

    Add multiple features for jobs engine like parent child dependencies, throttling with sleep between requests, timeout for processing

--- include/impl/jobs_item_impl.h
+++ include/impl/jobs_item_impl.h
@@ -10,15 +10,16 @@
 #include "../base_lock.h"
 
 namespace small::jobsimpl {
-    // a job can be in the following states
+    // a job can be in the following states (order is important because it may progress only to higher states)
     enum class EnumJobsState : unsigned int
     {
         kNone = 0,
         kInProgress,
+        kWaitChildren,
         kFinished,
+        kTimeout,
         kFailed,
         kCancelled,
-        kTimeout
     };
 
     // a job item
@@ -27,44 +28,49 @@ namespace small::jobsimpl {
     {
         using JobsID = unsigned long long;
 
-        JobsID                     id{};                        // job unique id
-        JobsTypeT                  type{};                      // job type
-        std::atomic<EnumJobsState> state{EnumJobsState::kNone}; // job state
-        std::atomic<int>           progress{};                  // progress 0-100 for state kInProgress
-        JobsRequestT               request{};                   // request needed for processing function
-        JobsResponseT              response{};                  // where the results are saved (for the finished callback if exists)
-        // TODO add parents and children ids
+        JobsID                     m_id{};                        // job unique id
+        JobsTypeT                  m_type{};                      // job type
+        std::atomic<EnumJobsState> m_state{EnumJobsState::kNone}; // job state
+        std::atomic<int>           m_progress{};                  // progress 0-100 for state kInProgress
+        std::vector<JobsID>        m_parentIDs{};                 // for dependencies relationships parent-child
+        std::vector<JobsID>        m_childrenIDs{};               // for dependencies relationships parent-child
+        JobsRequestT               m_request{};                   // request needed for processing function
+        JobsResponseT              m_response{};                  // where the results are saved (for the finished callback if exists)
 
         explicit jobs_item() = default;
         explicit jobs_item(const JobsID &jobs_id, const JobsTypeT &jobs_type, const JobsRequestT &jobs_request)
-            : id(jobs_id), type(jobs_type), request(jobs_request) {}
+            : m_id(jobs_id), m_type(jobs_type), m_request(jobs_request) {}
         explicit jobs_item(const JobsTypeT &jobs_type, const JobsRequestT &jobs_request)
-            : type(jobs_type), request(jobs_request) {}
+            : m_type(jobs_type), m_request(jobs_request) {}
         explicit jobs_item(const JobsID &jobs_id, const JobsTypeT &jobs_type, JobsRequestT &&jobs_request)
-            : id(jobs_id), type(jobs_type), request(std::forward<JobsRequestT>(jobs_request)) {}
+            : m_id(jobs_id), m_type(jobs_type), m_request(std::forward<JobsRequestT>(jobs_request)) {}
         explicit jobs_item(const JobsTypeT &jobs_type, JobsRequestT &&jobs_request)
-            : type(jobs_type), request(std::forward<JobsRequestT>(jobs_request)) {}
+            : m_type(jobs_type), m_request(std::forward<JobsRequestT>(jobs_request)) {}
 
         jobs_item(const jobs_item &other) { operator=(other); };
         jobs_item(jobs_item &&other) noexcept { operator=(other); };
         jobs_item &operator=(const jobs_item &other)
         {
-            id       = other.id;
-            type     = other.type;
-            state    = other.state.load();
-            progress = other.progress.load();
-            request  = other.request;
-            response = other.response;
+            m_id          = other.m_id;
+            m_type        = other.m_type;
+            m_state       = other.m_state.load();
+            m_progress    = other.m_progress.load();
+            m_parentIDs   = other.m_parentIDs;
+            m_childrenIDs = other.m_childrenIDs;
+            m_request     = other.m_request;
+            m_response    = other.m_response;
             return *this;
         }
         jobs_item &operator=(jobs_item &&other) noexcept
         {
-            id       = std::move(other.id);
-            type     = std::move(other.type);
-            state    = other.state.load();
-            progress = other.progress.load();
-            request  = std::move(other.request);
-            response = std::move(other.response);
+            m_id          = std::move(other.m_id);
+            m_type        = std::move(other.m_type);
+            m_state       = other.m_state.load();
+            m_progress    = other.m_progress.load();
+            m_parentIDs   = std::move(other.m_parentIDs);
+            m_childrenIDs = std::move(other.m_childrenIDs);
+            m_request     = std::move(other.m_request);
+            m_response    = std::move(other.m_response);
             return *this;
         }
 
@@ -74,27 +80,41 @@ namespace small::jobsimpl {
         inline void set_state(const EnumJobsState &new_state)
         {
             for (;;) {
-                EnumJobsState current_state = state.load();
+                EnumJobsState current_state = m_state.load();
                 if (current_state >= new_state) {
                     return;
                 }
-                if (state.compare_exchange_weak(current_state, new_state)) {
+                if (m_state.compare_exchange_weak(current_state, new_state)) {
                     return;
                 }
             }
         }
 
+        // clang-format off
+        inline void set_state_inprogress    () { set_state(EnumJobsState::kInProgress); }
+        inline void set_state_waitchildren  () { set_state(EnumJobsState::kWaitChildren); }
+        inline void set_state_finished      () { set_state(EnumJobsState::kFinished); }
+        inline void set_state_timeout       () { set_state(EnumJobsState::kTimeout); }
+        inline void set_state_failed        () { set_state(EnumJobsState::kFailed); }
+        inline void set_state_cancelled     () { set_state(EnumJobsState::kCancelled); }
+        
+        inline bool is_state_inprogress     () { return m_state.load() == EnumJobsState::kInProgress; }
+        inline bool is_state_finished       () { return m_state.load() == EnumJobsState::kFinished; }
+        inline bool is_state_timeout        () { return m_state.load() == EnumJobsState::kTimeout; }
+        // clang-format on
+
         //
         // set job progress (can only increase)
         //
+
         inline void set_progress(const int &new_progress)
         {
             for (;;) {
-                int current_progress = progress.load();
+                int current_progress = m_progress.load();
                 if (current_progress >= new_progress) {
                     return;
                 }
-                if (progress.compare_exchange_weak(current_progress, new_progress)) {
+                if (m_progress.compare_exchange_weak(current_progress, new_progress)) {
                     return;
                 }
             }
