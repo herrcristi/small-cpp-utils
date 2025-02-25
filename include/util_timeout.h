dbd57d755477fb91f7271ce311b826fb0f2c523b
Cristian Herghelegiu <herr.cristi@gmail.com>
2025-02-25 22:13:02 +0200

    Add set_timeout and set_interval to execute custom functions

--- include/util_timeout.h
+++ include/util_timeout.h
@@ -4,7 +4,7 @@
 #include <chrono>
 #include <thread>
 
-#include "jobs_engine.h"
+#include "impl/util_timeout_impl.h"
 
 //
 // set_timeout / set_timeout - helping function to execute a function after a specified timeout interval
@@ -17,98 +17,21 @@
 //
 
 namespace small {
-    namespace timeoutimpl {
-        enum class JobsType
-        {
-            kJobsTimeout = 0,
-            kJobsInterval,
-        };
-
-        enum class JobsGroupType
-        {
-            kJobsGroupDefault,
-        };
-
-        using TimeoutDuration = typename std::chrono::system_clock::duration;
-        using TimeoutRequest  = std::tuple<TimeoutDuration, void* /*engine*/, std::function<void()>>; // save the duration for the interval
-        using TimeoutResponse = bool;
-        using JobsEng         = small::jobs_engine<JobsType, TimeoutRequest, TimeoutResponse, JobsGroupType>;
-
-        // get the timeout engine
-        inline auto& get_timeout_engine()
-        {
-
-            // this functions is defined without the engine params (it is here just for the example)
-            static auto jobs_function_processing = [](const std::vector<std::shared_ptr<JobsEng::JobsItem>>& items, JobsEng::JobsConfig::ConfigProcessing& /* config */) {
-                for (auto& item : items) {
-                    auto& [timeout, engine, timeout_function] = item->m_request;
-                    timeout_function();
-                    item->m_response = true;
-
-                    // reschedule the interval
-                    if (item->m_type == JobsType::kJobsInterval) {
-                        small::timeoutimpl::JobsEng::JobsID intervalID{};
-
-                        auto* j = ((JobsEng*)engine);
-                        j->queue().push_back_and_start_delay_for(
-                            timeout,
-                            small::EnumPriorities::kNormal,
-                            small::timeoutimpl::JobsType::kJobsInterval,
-                            {timeout, engine, timeout_function},
-                            &intervalID);
-                    }
-                }
-            };
-
-            constexpr int threads_count = 4; // just like in node.js
-
-            static JobsEng::JobsConfig config{
-                .m_engine = {.m_threads_count = threads_count,
-                             .m_config_prio   = {.priorities = {{small::EnumPriorities::kNormal, 1}}}}, // overall config with default priorities
-
-                .m_default_function_processing = jobs_function_processing, // default processing function
-
-                // config by jobs group
-                .m_groups = {
-                    {JobsGroupType::kJobsGroupDefault, {.m_threads_count = threads_count}},
-                },
-
-                // config by jobs type
-                .m_types = {{JobsType::kJobsTimeout, {.m_group = JobsGroupType::kJobsGroupDefault}},    // timeout
-                            {JobsType::kJobsInterval, {.m_group = JobsGroupType::kJobsGroupDefault}}}}; // interval
-
-            static JobsEng jobs_timeout{config};
-
-            return jobs_timeout;
-        }
-    } // namespace timeoutimpl
-
     //
     // set_timeout - helping function to execute a function after a specified timeout interval
     // returns the timeoutID with which the timeout can be cancelled if not executed
     //
     template <typename _Callable, typename... Args>
-    inline unsigned long long set_timeout(const std::chrono::milliseconds& timeout, _Callable function_timeout, Args... function_parameters)
+    inline unsigned long long set_timeout(const typename std::chrono::system_clock::duration& timeout, _Callable function_timeout, Args... function_parameters)
     {
-        auto& jobs_timeout = timeoutimpl::get_timeout_engine();
-
-        timeoutimpl::JobsEng::JobsID timeoutID{};
-
-        auto ret = jobs_timeout.queue().push_back_and_start_delay_for(
-            timeout,
-            small::EnumPriorities::kNormal,
-            timeoutimpl::JobsType::kJobsTimeout,
-            {timeout, &jobs_timeout, std::bind(function_timeout, function_parameters...)},
-            &timeoutID);
-
-        return ret ? timeoutID : 0;
+        auto& timeout_engine = small::timeoutimpl::get_timeout_engine();
+        return timeout_engine.set_timeout(timeout, function_timeout, function_parameters...);
     }
 
     inline bool clear_timeout(const unsigned long long& timeoutID)
     {
-        auto& jobs_timeout = timeoutimpl::get_timeout_engine();
-
-        return jobs_timeout.state().jobs_cancelled(timeoutID);
+        auto& timeout_engine = small::timeoutimpl::get_timeout_engine();
+        return timeout_engine.clear_timeout(timeoutID);
     }
 
     //
@@ -116,27 +39,16 @@ namespace small {
     // returns the intervalID with which the interval can be cancelled if not executed
     //
     template <typename _Callable, typename... Args>
-    inline unsigned long long set_interval(const std::chrono::milliseconds& timeout, _Callable function_interval, Args... function_parameters)
+    inline unsigned long long set_interval(const typename std::chrono::system_clock::duration& timeout, _Callable function_interval, Args... function_parameters)
     {
-        auto& jobs_timeout = timeoutimpl::get_timeout_engine();
-
-        timeoutimpl::JobsEng::JobsID intervalID{};
-
-        auto ret = jobs_timeout.queue().push_back_and_start_delay_for(
-            timeout,
-            small::EnumPriorities::kNormal,
-            timeoutimpl::JobsType::kJobsInterval,
-            {timeout, &jobs_timeout, std::bind(function_interval, function_parameters...)},
-            &intervalID);
-
-        return ret ? intervalID : 0;
+        auto& timeout_engine = small::timeoutimpl::get_timeout_engine();
+        return timeout_engine.set_interval(timeout, function_interval, function_parameters...);
     }
 
     inline bool clear_interval(const unsigned long long& intervalID)
     {
-        auto& jobs_timeout = timeoutimpl::get_timeout_engine();
-
-        return jobs_timeout.state().jobs_cancelled(intervalID);
+        auto& timeout_engine = small::timeoutimpl::get_timeout_engine();
+        return timeout_engine.clear_interval(intervalID);
     }
 
     //
@@ -148,26 +60,26 @@ namespace small {
         //
         inline void signal_exit_force()
         {
-            return timeoutimpl::get_timeout_engine().signal_exit_force();
+            return small::timeoutimpl::get_timeout_engine().signal_exit_force();
         }
 
-        inline EnumLock wait()
+        inline small::EnumLock wait()
         {
-            return timeoutimpl::get_timeout_engine().wait();
+            return small::timeoutimpl::get_timeout_engine().wait();
         }
 
         // wait some time then signal exit
         template <typename _Rep, typename _Period>
-        inline EnumLock wait_for(const std::chrono::duration<_Rep, _Period>& __rtime)
+        inline small::EnumLock wait_for(const std::chrono::duration<_Rep, _Period>& __rtime)
         {
-            return timeoutimpl::get_timeout_engine().wait_for(__rtime);
+            return small::timeoutimpl::get_timeout_engine().wait_for(__rtime);
         }
 
         // wait until then signal exit
         template <typename _Clock, typename _Duration>
-        inline EnumLock wait_until(const std::chrono::time_point<_Clock, _Duration>& __atime)
+        inline small::EnumLock wait_until(const std::chrono::time_point<_Clock, _Duration>& __atime)
         {
-            return timeoutimpl::get_timeout_engine().wait_until(__atime);
+            return small::timeoutimpl::get_timeout_engine().wait_until(__atime);
         }
     } // namespace timeout
 
