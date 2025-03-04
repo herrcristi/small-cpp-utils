e91291e536921de63731bfb6826759c8a1d4c9b2
Cristian Herghelegiu <herr.cristi@gmail.com>
2025-03-05 00:47:39 +0200

    Solve compiler warnings

--- include/impl/impl_common.h
+++ include/impl/impl_common.h
@@ -2,7 +2,6 @@
 
 #if defined(_MSC_VER)
 #pragma warning(disable : 4464) // relative include path contains '..'
-#pragma warning(disable : 4577) // 'noexcept' used with no exception handling mode specified; termination on exception is not guaranteed
 #pragma warning(disable : 4711) // function selected for automatic inline expansion
 #pragma warning(disable : 4710) // function not inlined
 #pragma warning(disable : 4514) // unreferenced inline function has been removed
--- tests/test_common.h
+++ tests/test_common.h
@@ -2,6 +2,13 @@
 
 #if defined(_MSC_VER)
 #pragma warning(disable : 4464) // relative include path contains '..'
+
+// for tests only
+#pragma warning(disable : 4625) // '`anonymous-namespace'::...Test': copy constructor was implicitly defined as deleted
+#pragma warning(disable : 5026) // '`anonymous-namespace'::...Test': move constructor was implicitly defined as deleted
+#pragma warning(disable : 4626) // '`anonymous-namespace'::...Test': assignment operator was implicitly defined as deleted
+#pragma warning(disable : 5027) // '`anonymous-namespace'::...Test': move assignment operator was implicitly defined as deleted
+
 #endif
 
 #include "../include/impl/impl_common.h"
