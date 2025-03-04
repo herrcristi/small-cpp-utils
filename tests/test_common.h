7555962015d41b109269a80b7bb9346c200aa400
Cristian Herghelegiu <herr.cristi@gmail.com>
2025-03-04 23:49:12 +0200

    Solve compiler warnings

--- tests/test_common.h
+++ tests/test_common.h
@@ -2,6 +2,7 @@
 #pragma warning(disable : 4464) // relative include path contains '..'
 #pragma warning(disable : 4577) // 'noexcept' used with no exception handling mode specified; termination on exception is not guaranteed
 #pragma warning(disable : 4711) // function selected for automatic inline expansion
+#pragma warning(disable : 4710) // function not inlined
 #endif
 
 #include <gtest/gtest.h>
